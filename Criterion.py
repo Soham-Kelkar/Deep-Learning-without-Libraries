e6327e814f86d453b3eaa0ae0a0b722a6474ac5c
Soham Kelkar <29782946+Soham-Kelkar@users.noreply.github.com>
2019-02-27 09:04:43 -0500

    Create Criterion.py

---
+++ Criterion.py
@@ -0,0 +1,17 @@
+class Criterion(object):
+    """ Interface for loss functions.
+    """
+
+    def __init__(self):
+        self.logits = None
+        self.labels = None
+        self.loss = None
+
+    def __call__(self, x, y):
+        return self.forward(x, y)
+
+    def forward(self, x, y):
+        pass
+
+    def derivative(self):
+        pass
